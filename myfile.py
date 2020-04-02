65e7a2ad34190ac35ff355ae9c1710d77b2f8340
mithem <mithem@github.com>
2020-04-02 11:34:57 +0200

    Coronavirus? Yep. Not really.

--- myfile.py
+++ myfile.py
@@ -4,30 +4,20 @@ CONFIG = {
     "World": {
         "initial_food": 1000,
         "initial": {
-            "n_Cat": 100,
-            "n_Dog": 100
+            "n_Coronavirus": 100,
         },
         "food_function": functions.base
     },
     "Creatures": {
-        "Cat": {
+        "Coronavirus": {
             "age_increments": 1,
             "energy": 10,
-            "reproduction_chance": 0.12
-        },
-        "Dog": {
-            "age_increments": 1,
-            "energy": 10,
-            "reproduction_chance": 0.1175
+            "reproduction_chance": 0.13
         }
     },
     "iterations": 1000
 }
 
 
-class Cat(Creature):
-    pass
-
-
-class Dog(Creature):
+class Coronavirus(Creature):
     pass
